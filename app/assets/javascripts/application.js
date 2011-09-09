dc4ecef8f03f300725ddd77d15f33ab40c2bcd9d
Drew Neil <andrew.jr.neil@gmail.com>
2011-09-09 17:40:50 +0100

    Load all required javascripts for Touch app.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,6 @@
 //= require sencha-touch
-//= require_tree .
+//= require "app/app.js"
+//= require_tree "../../../lib/assets/javascripts/app/models"
+//= require_tree "../../../lib/assets/javascripts/app/stores"
+//= require_tree "../../../lib/assets/javascripts/app/views"
+//= require_tree "../../../lib/assets/javascripts/app/controllers"
