ea34c1b57b0b9b17ad43b1b4c9724c7f6ec92e9d
Drew Neil <andrew.jr.neil@gmail.com>
2011-09-09 16:46:55 +0100

    Initialize rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SenchaOnRails::Application.config.secret_token = '79363a3405090c9dbf7288d70a21ffad7b03954de397c7c355aa7bf441f4d3fd3f7d296d28bc1ecb8eaea9aca4cbe17b0a93e1f7e8f014e2c56619ff0755b1db'
