6f21147ecd820597be2a607f8b64ad6456e0f3ea
Drew Neil <andrew.jr.neil@gmail.com>
2011-09-13 17:08:29 +0100

    Migrate database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110909174036) do
+ActiveRecord::Schema.define(:version => 20110913160216) do
 
   create_table "users", :force => true do |t|
-    t.string   "name"
+    t.string   "username"
     t.string   "email"
-    t.string   "phone"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
