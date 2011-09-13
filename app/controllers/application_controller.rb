3c87fbc3e902120299f19ac13a67e4ab162f2842
Drew Neil <andrew.jr.neil@gmail.com>
2011-09-13 19:27:35 +0100

    Re-enable CSRF - and raise error on detection.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   include ControllerAuthentication
-  # protect_from_forgery
+  protect_from_forgery
+
+  def handle_unverified_request
+    raise(ActionController::InvalidAuthenticityToken)
+  end
+
 end
