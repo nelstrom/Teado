69b2cf0e631b891a1cb608d59626e7b606897142
Drew Neil <andrew.jr.neil@gmail.com>
2011-09-13 17:11:08 +0100

    Use get, not match, for routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 SenchaOnRails::Application.routes.draw do
-  match 'user/edit' => 'users#edit', :as => :edit_current_user
 
-  match 'signup' => 'users#new', :as => :signup
-
-  match 'logout' => 'sessions#destroy', :as => :logout
-
-  match 'login' => 'sessions#new', :as => :login
+  get 'user/edit' => 'users#edit', :as => :edit_current_user
+  get 'signup' => 'users#new', :as => :signup
+  get 'logout' => 'sessions#destroy', :as => :logout
+  get 'login' => 'sessions#new', :as => :login
 
   resources :sessions
-
   resources :users
 
   get "touch/index"
   root :to => 'sessions#new'
+
 end
