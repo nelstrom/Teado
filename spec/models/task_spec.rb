f64a72a2063e7b6cfc7c326d02a04970ed8aeb55
Drew Neil <andrew.jr.neil@gmail.com>
2011-09-28 12:42:09 +0100

    Rephrase the specs a little.

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 
 describe Task do
 
-  it "should create a new task instance given valid attributes" do
+  it "creates a new task given valid attributes" do
     task = Factory(:task)
     task.should be_valid
     task.errors.should be_empty
   end
 
-  it "should create a task with due date" do
-    task = Factory(:task, :due_at => Time.now.end_of_week, :bucket => "due_this_week")
+  it "calculates the due date from the 'bucket' text" do
+    task = Factory(:task, :bucket => "due_this_week")
     task.errors.should be_empty
     task.bucket.should == "due_this_week"
     task.due_at.should == Time.zone.now.end_of_week
   end
 
-  it "should update due date" do
+  it "updates the due date when assigned to a new 'bucket'" do
     task = Factory(:task, :due_at => Time.now.midnight.tomorrow, :bucket => "due_tomorrow")
     task.update_attributes( { :bucket => "due_this_week" } )
     task.errors.should be_empty
