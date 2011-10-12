e2025155b46dff3ee396ceb85dd2451f6a31f1f2
Drew Neil <andrew.jr.neil@gmail.com>
2011-10-12 15:36:41 +0100

    Install sencha-touch compass theme. Twice.

---
+++ vendor/plugins/compass_extensions/sencha-touch/templates/project/manifest.rb
@@ -0,0 +1,9 @@
+stylesheet 'sencha-touch.scss'
+
+help %Q{
+This is a message that users will see if they type
+
+  compass help sencha-touch
+
+You can use it to help them learn how to use your extension.
+}
