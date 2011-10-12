6ce1f38b6b1b7733a11637203a94c61b25891fb3
Drew Neil <andrew.jr.neil@gmail.com>
2011-10-12 17:22:55 +0100

    Uninstall ad-hoc sencha-touch compass plugin.

--- vendor/plugins/compass_extensions/sencha-touch/templates/project/manifest.rb
+++
@@ -1,9 +0,0 @@
-stylesheet 'sencha-touch.scss'
-
-help %Q{
-This is a message that users will see if they type
-
-  compass help sencha-touch
-
-You can use it to help them learn how to use your extension.
-}
