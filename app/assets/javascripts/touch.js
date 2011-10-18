b7d0b306e53e6c8c4d614b75549206a2eb98eb76
Drew Neil <andrew.jr.neil@gmail.com>
2011-10-18 11:30:45 +0100

    Move entire Sencha Touch app inside app/ directory.

---
+++ app/assets/javascripts/touch.js
@@ -0,0 +1,8 @@
+//= require      "sencha-touch"
+//= require      "app/routes"
+//= require      "app/app"
+//= require      "app/overrides"
+//= require_tree "./app/models"
+//= require_tree "./app/stores"
+//= require_tree "./app/views"
+//= require_tree "./app/controllers"
