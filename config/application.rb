0cdb36bc04b3de56c4ff728fa9c63f950f16be18
Drew Neil <andrew.jr.neil@gmail.com>
2011-10-20 16:43:53 +0100

    Require and use Rack::JSONP middleware.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'rack/contrib'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -14,6 +15,8 @@ module SenchaOnRails
     config.autoload_paths << "#{config.root}/lib"    # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.use Rack::JSONP
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
